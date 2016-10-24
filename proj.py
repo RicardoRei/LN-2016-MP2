1cba0b5d98c274a9ea6296335f88346c69d2197d
Miguel Carvalho <migueloak59@hotmail.com>
2016-10-24 18:54:20 +0100

    evaluationFormatting is now called writeToFile and creates a .txt from the input filename with the results

--- proj.py
+++ proj.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import re
+import os
 import nltk
 from nltk import bigrams
 from nltk import trigrams
@@ -37,16 +38,24 @@ def countNgramFrequency(ngram_tokens):
 
 
 #prints output according to professors' specification	
-def evaluationFormatting(countedNgram):
+def writeToFile(countedNgram,filename):
+	indexNr = filename.index('.')
+	newFilename=filename[:indexNr]+"out.txt"
+	
+	if os.path.isfile(newFilename):
+		os.remove(newFilename)
+		
+	os.mknod(newFilename)
+	outputFile=open(newFilename,"w")
 	for element in countedNgram:
 		new=""
 		for i in element[1]:
 			new=new+i+" "
-		print "%s%d"%(new,element[0])
-
+		outputFile.write(new +" "+str(element[0])+"\n")
 
 cmdargs=sys.argv
 fileIn = open(cmdargs[1], 'rU')
+#fileIn = open('Corpora/test.txt', 'rU')
 processed_text = processPunctuation(fileIn)
 fileIn.close()
 print (processed_text)
@@ -54,8 +63,10 @@ print (processed_text)
 # split the texts into tokens
 tokens = nltk.word_tokenize(processed_text)
 tokens = [token.lower() for token in tokens if (len(token) > 1)] #same as unigrams
+tri_tokens=nltk.trigrams(tokens)
 bi_tokens = nltk.bigrams(tokens)
 	
 counted_bigram = countNgramFrequency(list(bi_tokens))
-	
-evaluationFormatting(counted_bigram)
+counted_trigram=countNgramFrequency(list(tri_tokens))
+
+writeToFile(counted_bigram,cmdargs[1])
